704eddffaa751954678124ab47cce115bc520e05
eric <erictoribio2010@gmai.com>
2022-10-02 22:18:02 -0400

    CHANGED LANDING LINKS

--- client/navigation/nav-bars/landing-nav.tsx
+++ client/navigation/nav-bars/landing-nav.tsx
@@ -1,11 +1,54 @@
 
 import Image from 'next/image'
+import { useState } from 'react';
 import logo from '../../static/img/toribio-ecommerce.png' 
-export default () => {
+export default (props : {page : string}) => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  
 
+    const style = {
+      "display": "none"
+    }
+    const handleNull = () => { return null;}
+   
+  
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed)
+  
     return(
-        <div className='nav-container'>
-           <Image width={250} height={100} src={logo} alt="Toribio's Ecom"/>
+        <nav className="nav-container navbar navbar-expand-lg navbar-dark  rounded px-4">
+            <div id='nav-logo'>
+           <Image  width={250} height={100} src={logo} alt="Toribio's Ecom"/>
+            </div>
+           <button className="custom-toggler navbar-toggler" type="button"
+        data-toggle="collapse"
+        data-target="#navbars"
+        aria-controls="navbars"
+        aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation"
+        data-auto-close="outside"
+        onClick={handleNavCollapse}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+           <div className={`${isNavCollapsed ? 'collapse ' : ''} navbar-collapse `} id="navbars">
+        <div className={`${isNavCollapsed ? 'navbar-collapse' : 'nav-dropdown '}  `}
+        onMouseLeave={isNavCollapsed ? handleNull : handleNavCollapse}>
+        
+            <a className="nav-link " href="/food" >Food</a>
+        
+         
+            <a href="/toys" className=" nav-link" >Toys</a>
+         
+            <a href="/containment" className=" nav-link " >Crates & Carriers</a>
+         
+            <a className=" nav-link " href='/furniture' >Beds & Furniture</a>
+        
+            <a href="/cleaning" className=" nav-link" >Cleaning Supplies</a>
+          
+          
+            <a className="nav-link " href="/collars" > Collars , Harnesses & Leashes</a>
+          
         </div>
+      </div>
+        </nav>
     )
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,7 @@ export default () => {
   return(
     <div  className="landing-container">
       <div className="nav-wrapper">
-      <LandingNav/>
+      <LandingNav page="landing"/>
       </div>
       <div  className="landing-body">
       <Landing />

411ffc941799b132688721570b501a0e5acb7d59
Eric <erictoribio2010@gmail.com>
2022-10-13 17:33:48 -0400

    laptop push

--- client/navigation/side-bars/sideBarComponents/dogSideBar.tsx
+++ client/navigation/side-bars/sideBarComponents/dogSideBar.tsx
@@ -1,10 +1,21 @@
+import { NavItem } from "react-bootstrap"
+import { SideBarTabs } from "../../../utils/side-bar-util"
 
 
 export default () => {
 
-    return(
-        <div>
-            <h1>Dogs</h1>
+    return (
+        <div className="d-flex  h-100">
+            <ul className="list-unstyled ">
+
+                {SideBarTabs.map((item, i) => {
+                    return (
+                        <li className="d-flex justify-content-center">
+                            <button className="side-bar-item">{item.Name}</button>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
---
+++ client/utils/side-bar-util.ts
@@ -0,0 +1,11 @@
+interface sideBarTab{
+    Name: string;
+    Tab: string;
+}
+export const SideBarTabs : sideBarTab[] =[
+    {Name : "Food" , Tab: "food" },
+    {Name: "Toys" , Tab: "toys" },
+    {Name: "Bedding" , Tab: "bedding" },
+    {Name: "Collars, Harnesses & Leases", Tab: "restraints"}
+    
+]

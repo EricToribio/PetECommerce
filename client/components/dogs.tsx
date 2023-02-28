650b940422162c4655ce37a309a73aecb24844ea
Eric <erictoribio2010@gmail.com>
2023-02-27 19:37:20 -0500

    playing with scroller

--- client/components/dogs.tsx
+++ client/components/dogs.tsx
@@ -1,6 +1,6 @@
 
 import NonSSRWrapper from '../utils/NoSSR'
-import { DogImageList, DogImageList2, images} from '../utils/dog-pic-utils'
+import { DogImageList, DogImageList2, images, netflixStyle} from '../utils/dog-pic-utils'
 import DogFood from './dogFood'
 import DogFoodTest from './DogFoodTest'
 
@@ -10,6 +10,7 @@ export default () => {
     let i2 = 0
     let g2 = 1
     let g = 1
+    let sec = 1
     
 
     return (
@@ -18,18 +19,19 @@ export default () => {
 
                 <DogFoodTest/>
             <DogFood/>
-            <div className="dog-body">
+            <div className="">
                 <h1>Food 2</h1>
-                <div className="media-container">
-                    <div className="media-scroller">
+                {/* <div className="media-container"> */}
+                    <div className="wrapper">
                         { DogImageList2.map((item, index) =>{ 
                             if(i < DogImageList2.length){
                                 if(index !== 0){
                                     g2+=1
-                                    i2+=4    
+                                    i2+=4
+                                    sec+=1    
                                 }
                                 return(
-                                    images(DogImageList2,i2,g2,'food2') ,
+                                    netflixStyle(DogImageList2,i2,g2,'food2',sec)
                                     )  
                                 } 
                         })
@@ -40,11 +42,11 @@ export default () => {
                             {/* <!-- 4 Groups --> */}
                            
                         </div>
-                    </div>
+                    {/* </div> */}
                 </div>
             </div>
-            <DogFoodTest/>
-            <svg>
+            
+            {/* <svg>
                 <symbol id="next" viewBox="0 0 256 512">
                     <path fill="white"
                         d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" />
@@ -53,7 +55,7 @@ export default () => {
                     <path fill="white"
                         d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" />
                 </symbol>
-            </svg>
+            </svg> */}
 
         </div>
         </NonSSRWrapper>
--- client/utils/dog-pic-utils.tsx
+++ client/utils/dog-pic-utils.tsx
@@ -335,3 +335,94 @@ export const images= (item: imageType[], j : number, g :number, name : String ) 
 
         }
 }
+
+export const netflixStyle = (item: imageType[], j : number, g :number, name : String , sec: number) => {
+
+    for (let i = j; i < item.length; i++) {
+            if (j === 0) {
+                return(
+                <section id={`section${sec}`}>
+                <a href={`#section${sec - 1} `}className="arrow__btn left-arrow">‹</a>
+                <div className="item">
+                  <a href="#">
+            <Image
+            src={item[0].Image} alt=""/>
+              <h1 className="heading">Heading One</h1>
+              <p className="duration">Duration: 10 min</p>
+                  </a>
+                </div>
+                <div className="item">
+            
+                  <a href="#">
+                  <Image src={item[1].Image} alt=""/>
+            
+            <h1 className="heading">Heading One</h1>
+            <p className="duration">Duration: 10 min</p>
+            
+                  </a>
+                </div>
+                <div className="item">
+                  
+                  <a href="#">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABQCoK53qihwVPLRxPEDX98nyYpGbxgi5cc0ZOM4iHQu7KQvtgNyaNM5PsgI0vy5g3rLPZdjGCFr1EggrCPXpL77p2H08jV0tNEmIfs_e8KUfvBJ6Ay5nM4UM1dl-58xA6t1swmautOM.webp"
+            alt="Describe Image"/>
+            <h1 className="heading">Heading One</h1>
+            <p className="duration">Duration: 10 min</p>
+                  </a>
+                </div>
+                <div className="item">
+                  <a href="#">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABdYtAqj8CyaJTWq5taD8Ro_UgwH3nne9QpFGVps-2J3IG-leqrfqXFii4jzZn48nPYTkrlwKQEV0R7_cEKlYBPRzdKqNODc-Oz26IL3LlLgFboXibIWXwxzeYxzuqn0I9TpARjeByw.jpg"
+            alt="Describe Image"/>
+            
+            <h1 className="heading">Heading One</h1>
+            <p className="duration">Duration: 10 min</p>
+                  </a>
+                </div>
+                <div className="item">
+                  <a href="#">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABbcCX42tsqGbBvO2y9CQv5-7QvYbCfoHtXsuc6NPCtZaKa4l4fBX3XWvUwG9F2A3CTsNpHVmulxBbdXKwK8Q6xGjejd9FoadGkZ7CnGrSl601TOQjzSHJ23NuIPC8j0QMGORL4uRIA.jpg"
+            alt="Describe Image"/>
+            <h1 className="heading">Heading One</h1>
+            <p className="duration">Duration: 10 min</p>
+            </a>
+                </div>
+                <a href={`#section${sec+1}`} className="arrow__btn right-arrow">›</a>
+              </section>)
+            } else {
+                <section id={`section${sec}`}>
+                <a href={`#section${sec-1}`} className="arrow__btn left-arrow">‹</a>
+                <div className="item">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABVopDZ5Fy9_fk_HO5WxHTXKKjKhtWFupbSjuvPwfLK_vytzon4EwRUdGgYJ34JwPxOTK_NkV3aXfkULMB0Dcct-FyfqzH-X44VXuRMp4QeBHlvKwWeZFpZlGdItPzmmg4scmwhG7SQ.jpg"
+            alt="Describe Image"/>
+                </div>
+                <div className="item">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABTOj1-116yVcgKWMU2dI3GFR4x0fSkiGsqtLLeLUxRR7STaksjAqBTrYlTfrB8nIGnGvXksi0ewXAhVGg6-pLxpFOIfcpjK-pf8D5xehFZo5a6vJbo4L0AGbrzglbyUoq255QBJgRQ.jpg"
+            alt="Describe Image"/>
+                </div>
+                <div className="item">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABd3IBDpxbRcHXvRMFCZeKa2aHLU1P4SJtrACMS9om3yhLjqPlvNlmR_fypPxjtbsbnKaC4JZhPSpDG4r_kdxSHHAltWguMcCB-1Y1OShr2zWfUv7Whf_39fNH5ZJ3_0gxQrs0akmQjQz44_LT7jXH5LMZ7iMBAzac5IEj4m7Fn_5OWEGYnVsDsKG-QTommDooULMDF9bEw.jpg"
+            alt="Describe Image"/>
+                </div>
+                <div className="item">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABXSd7bhDddcwkq9XpksoQFCHVx29Sxl_h4hb2n3F2GIt32a4XWcOnctQfgnT5qdolv8UML6_xNB5CJ89h56wueb13mYmEBr0sx5e9iLPdtVcOQAOmKXKWHHXwFvJuCUwuNnL3s8eAQwqLXPVMHMEsujM684rUGrZNF2btN2GRy5-RyEslsxZO93V2Q_H2bWs8A8oayt1h5M.webp"
+            alt="Describe Image"/>
+                </div>
+                <div className="item">
+                  <img
+            src="https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/a76057bcfd003711a76fb3985b1f2cf74beee3b8/AAAABbXWODpAWqVXcmmjMA7K-2mPkQpvwCLfSdeyhVXzR8A3MSpdSEnnjf4HEJJTYC-TnktU6njTUGAxmzWEYCaJbk4v_ZeL-7QGzmkvYBjg_N-evr2XmcX-Fanoyvu_nimFP4iigPe4O3Vr_WcgplhwkDrJwPUJa84wRLrNAx3TufN5V7cWRP4indqu5HQahvgFEqfL9zjp4g.jpg"
+            alt="Describe Image"/>
+                </div>
+                <a href={`#section${sec+1}`} className="arrow__btn right-arrow">›</a>
+              </section>
+            }
+        }
+
+}
